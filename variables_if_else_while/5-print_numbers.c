d2ef62e481c0b35106faa0d4c48f79bb333dd6e4
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-17 06:08:15 -0700

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
