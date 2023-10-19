a31f30ee5e9b6876e29fc8ee65f16aec3cbaca55
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-19 01:06:29 -0700

    2. 10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet_x10(void)
+{
+	char(c);
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
