af05b45e5f24ecec22f9d8aff487370e8791aa27
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-20 04:39:15 -0700

    2. Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 integers.
+ * @a: integer
+ * @b: inetegr
+ * Return:1 if c is uppercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
