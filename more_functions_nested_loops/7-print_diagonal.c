00e10552d4daf3c5d8e15f0c5791619f1ada4be9
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-21 10:15:26 -0700

    betty qardasimsan

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,24 +8,23 @@
 
 void print_diagonal(int n)
 {
-        int i;
+	int i;
 	int j;
 
-        if (n >= 0)
-        {
+	if (n > 0)
+	{
 		for (j = 0; j < n; j++)
 		{
-        for (i = 0; i < j; i++)
-        {
-		_putchar(' ');
-        }
-	_putchar(92);
-	_putchar('\n');
-        }
+			for (i = 0; i < j; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
-        else
-        {
-                _putchar('\n');
-        }
+	else
+	{
+		_putchar('\n');
+	}
 }
-
