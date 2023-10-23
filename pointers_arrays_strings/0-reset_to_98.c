b15e6f3ffd83a5dadddecb1b928af43c75ac6f90
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-23 00:25:52 -0700

    0. 98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - pointer to an int as parameter and points to to 98.
+ *@n: integr
+ * Return: 0 Succesfull
+ */
+void reset_to_98(int *n)
+{
+	int c;
+
+	*n = 98;
+	n = &c;
+}
