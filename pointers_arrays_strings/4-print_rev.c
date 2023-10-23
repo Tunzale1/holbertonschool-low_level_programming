b20feb128d75740a0a19208798f194e4909ed069
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-23 12:21:04 -0700

    4. I can only go one way. I've not got a reverse gear

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/** 
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	/* using for find length of dtring and find last character*/
+	while (*s != '\0')
+	{
+		length++;
+		++s;
+	}
+	/* using for go back to null charcater*/
+	s--;
+	/* using for print reverse*/
+	for (i = length; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
