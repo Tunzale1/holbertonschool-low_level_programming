04999804bdf541c0aaf1f54391e9791d2199cd6e
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-24 02:04:47 -0700

    Must work!

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	startFromHalf = length / 2;
 	if (length % 2 != 0)
 	{
-		startFromHalf = length - 1;
+		startFromHalf = (length - 1) / 2;
 	}
 	for (i = startFromHalf; i < length; i++)
 	{
