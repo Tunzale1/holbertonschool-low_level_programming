d79eb8ea1d57cf867b00c563fea8f997147870ef
Tunzale1 <tunzaleimanova03@mail.ru>
2023-10-30 13:01:47 -0700

    betty style solved

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 		_putchar('\n');
 	}
-	return _strlen_recursion(s + 1) + 1;
+	return (_strlen_recursion(s + 1) + 1);
 }
