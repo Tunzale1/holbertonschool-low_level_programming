864488a4e0ff88689b540901103433a2cbe1f63a
Tunzale1 <tunzaleimanova03@mail.ru>
2023-11-14 10:49:11 -0800

    predefined macros

---
+++ preprocessor/02-main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("file name :%s\n", __FILE__);
+	return (0);
+}
---
+++ preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - func
+ *Return: noth
+ */
+int main(void)
+{
+	printf("file name :%s\n", __FILE__);
+	return (0);
+}
