3d63aa400b71b7a98b7e8f2a1dad58290716942d
Tunzale1 <tunzaleimanova03@mail.ru>
2023-11-14 02:37:37 -0800

    qutadiimmm

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,10 +11,10 @@ int *array_range(int min, int max)
 	int *arr;
 	int i, size;
 
-	size = max - min + 1;
-	arr = malloc(sizeof(int) * size);
 	if (min > max)
 		return (NULL);
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
