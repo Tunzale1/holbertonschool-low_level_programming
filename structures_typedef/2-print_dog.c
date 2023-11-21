be8563b7cebeec8107bc40b5c75490ec09ec6273
Tunzale1 <tunzaleimanova03@mail.ru>
2023-11-20 23:14:59 -0800

    nolar

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
  * print_dog - function that prints a struct dog
  * @d: pointer
@@ -9,15 +10,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		puts("Name: (nil)\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age < 0)
-		puts("Age: (nil)\n");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		puts("Owner: (nil)\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
