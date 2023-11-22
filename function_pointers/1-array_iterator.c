3d412cbdaca83f77514af63b14bb31bafe4a298a
Tunzale1 <tunzaleimanova03@mail.ru>
2023-11-22 03:08:11 -0800

    oldu bu defe

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL)
 		return;
-	if(action == NULL)
+	if (action == NULL)
 		return;
 	for (i = 0; i < size; i++)
 		action(array[i]);
