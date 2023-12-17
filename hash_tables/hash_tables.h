f8171b9cbd465311a15a1d0b1a502cc66ccc0747
Tunzale1 <tunzaleimanova03@mail.ru>
2023-12-17 07:52:52 -0800

    :rocket:

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,8 @@
 #ifndef hash
 #define hash
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -10,9 +13,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,12 +28,11 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
