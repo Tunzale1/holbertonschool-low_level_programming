9a07c70ae77ad7cb38533e8ed900d97463a7085b
Tunzale1 <tunzaleimanova03@mail.ru>
2023-12-17 08:41:22 -0800

    :rocket:

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,8 +6,6 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i = 0;
-
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (!ht)
@@ -15,12 +13,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (!ht->array)
-	{
-		free(ht);
-		return (NULL);
-	}
-	for (; i < size; i++)
-		ht->array[i] = NULL;
+
 	return (ht);
 }
