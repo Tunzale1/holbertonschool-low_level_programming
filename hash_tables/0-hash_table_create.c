64dce030d92520670d026f9f076804fdc9275bea
Tunzale1 <tunzaleimanova03@mail.ru>
2023-12-17 07:47:36 -0800

    init

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size: size of array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (; i < size; i++)
+		ht->array[i] = NULL;
+	return (ht);
+}
