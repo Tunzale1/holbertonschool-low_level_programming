24a0529f19860b98ef6cc4cbe38fcd3a3e46a693
Tunzale1 <tunzaleimanova03@mail.ru>
2024-01-18 08:05:33 -0800

    header file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,22 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @index: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-
-typedef struct listint_s
-{
-		int n;
-		size_t index;
-		struct listint_s *next;
-} listint_t;
-
-
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
